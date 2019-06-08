7d917b39ca58042ad0c3ac81b8d5d5325c82e605
Dennis <dennissarmiento808@gmail.com>
2019-06-08 01:52:25 -0700

    up

---
+++ build/precache-manifest.2e02220f0d45bc45f610776f26e34482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb1326dbcb212b0664e8e449386d0fa",
+    "url": "/my-portfolio-react/index.html"
+  },
+  {
+    "revision": "7f0f9829a3604f4cb740",
+    "url": "/my-portfolio-react/static/css/main.a08a18b6.chunk.css"
+  },
+  {
+    "revision": "153d97f37e7f3ae3b11f",
+    "url": "/my-portfolio-react/static/js/2.694b9560.chunk.js"
+  },
+  {
+    "revision": "7f0f9829a3604f4cb740",
+    "url": "/my-portfolio-react/static/js/main.b88cd96b.chunk.js"
+  },
+  {
+    "revision": "6e85564cccd5bd7615b8",
+    "url": "/my-portfolio-react/static/js/runtime~main.94338e10.js"
+  },
+  {
+    "revision": "473ef91ea91d92be0d4016f15bdc570b",
+    "url": "/my-portfolio-react/static/media/DS2.473ef91e.png"
+  },
+  {
+    "revision": "6f69d94ae9e8ec66d3ec6a0f2f476a51",
+    "url": "/my-portfolio-react/static/media/DS3.6f69d94a.png"
+  }
+]);
