ab151e3f6df0a17453409e7fb96851e3ef3a6994
Dennis <dennissarmiento808@gmail.com>
2019-06-23 16:24:58 +0800

    up

--- build/precache-manifest.7048368f80ed5afbc81fb6cb2a77cd64.js
+++ build/precache-manifest.7048368f80ed5afbc81fb6cb2a77cd64.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "389aa38bbf9ce8b42db01ee0654e1da3",
+    "revision": "4c492aae0b03599eabad17b9f60d1723",
     "url": "/my-portfolio-react/index.html"
   },
   {
-    "revision": "bd42843512c3794b307b",
-    "url": "/my-portfolio-react/static/css/main.a18892c2.chunk.css"
+    "revision": "b5c208cb9256205932c9",
+    "url": "/my-portfolio-react/static/css/main.11d12e8c.chunk.css"
   },
   {
-    "revision": "76e1d15465f833dc85ca",
-    "url": "/my-portfolio-react/static/js/2.29a4621f.chunk.js"
+    "revision": "b931e7418eb518df485f",
+    "url": "/my-portfolio-react/static/js/2.1b9f5a65.chunk.js"
   },
   {
-    "revision": "bd42843512c3794b307b",
-    "url": "/my-portfolio-react/static/js/main.c3dfd902.chunk.js"
+    "revision": "b5c208cb9256205932c9",
+    "url": "/my-portfolio-react/static/js/main.78dced02.chunk.js"
   },
   {
     "revision": "6e85564cccd5bd7615b8",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "aee0f87f19a0b29206e529199c9f6f8b",
     "url": "/my-portfolio-react/static/media/DS3NOBGBG.aee0f87f.png"
+  },
+  {
+    "revision": "11dbaacfd9eb9a00a57bcd9661f6eaf0",
+    "url": "/my-portfolio-react/static/media/DennisSarmientoResumeWebDev3.11dbaacf.pdf"
   },
   {
     "revision": "3da2c9c3a21fde2f087b62fc6d08d415",
